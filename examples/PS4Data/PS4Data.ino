78550397923d3195684812d60626f60e83719833
Pablo Marquez <92705318+pablomarquez76@users.noreply.github.com>
2023-09-09 09:55:41 -0300

    Added options to show events, buttons, joysticks or sensors

--- examples/PS4Data/PS4Data.ino
+++ examples/PS4Data/PS4Data.ino
@@ -1,5 +1,9 @@
 #include <PS4Controller.h>
 unsigned long lastTimeStamp = 0;
+#define EVENTS 0
+#define BUTTONS 0
+#define JOYSTICKS 1
+#define SENSORS 0
 
 void setup() {
   Serial.begin(115200);
@@ -7,72 +11,66 @@ void setup() {
   PS4.attachOnConnect(onConnect);
   PS4.attachOnDisconnect(onDisConnect);
   PS4.begin();
-  Serial.println("Ready.");
+  Serial.println("Ready!");
 }
 
 void loop() {
+  delay(100);
 }
 
 void onConnect() {
-  Serial.println("Connected!.");
+  Serial.println("Connected!");
 }
 
 void notify() {
-  char messageString[255];
+#if EVENTS
+  boolean sqd = PS4.event.button_down.square,
+          squ = PS4.event.button_up.square,
+          trd = PS4.event.button_down.triangle,
+          tru = PS4.event.button_up.triangle;
+  if (sqd)
+    Serial.println("SQUARE down");
+  else if (squ)
+    Serial.println("SQUARE up");
+  else if (trd)
+    Serial.println("TRIANGLE down");
+  else if (tru)
+    Serial.println("TRIANGLE up");
+#endif
 
-  sprintf(messageString, "%3d,%3d,%3d,%3d,%3d,%3d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d,%2d",
-          PS4.LStickX(),
-          PS4.LStickY(),
-          PS4.RStickX(),
-          PS4.RStickY(),
-          PS4.L2Value(),
-          PS4.R2Value(),
-          PS4.Left(),
-          PS4.Down(),
-          PS4.Right(),
-          PS4.Up(),
-          PS4.Square(),
-          PS4.Cross(),
-          PS4.Circle(),
-          PS4.Triangle(),
-          PS4.L1(),
-          PS4.R1(),
-          PS4.L2(),
-          PS4.R2(),
-          PS4.L3(),
-          PS4.R3(),
-          PS4.Share(),
-          PS4.Options(),
-          PS4.PSButton(),
-          PS4.Touchpad(),
-          PS4.Charging(),
-          PS4.Audio(),
-          PS4.Mic(),
-          PS4.Battery());
-		  
-  /*
-  sprintf(messageString, "gx:%d,gy:%d,gz:%d,ax:%d,ay:%d,az:%d,",
-          PS4.GyrX(),
-          PS4.GyrY(),
-          PS4.GyrZ(),
-          PS4.AccX(),
-          PS4.AccY(),
-          PS4.AccZ());
-
-  sprintf(messageString, "lx:%d,ly:%d,rx:%d,ry:%d,",
-          PS4.LStickX(),
-          PS4.LStickY(),
-          PS4.RStickX(),
-          PS4.RStickY());
-  */
+#if BUTTONS
+  boolean sq = PS4.Square(),
+          tr = PS4.Triangle();
+  if (sq)
+    Serial.print(" SQUARE pressed");
+  if (tr)
+    Serial.print(" TRIANGLE pressed");
+  if (sq | tr)
+    Serial.println();
+#endif
 
   //Only needed to print the message properly on serial monitor. Else we dont need it.
   if (millis() - lastTimeStamp > 50) {
-    Serial.println(messageString);
+#if JOYSTICKS
+    Serial.printf("lx:%4d,ly:%4d,rx:%4d,ry:%4d\n",
+                  PS4.LStickX(),
+                  PS4.LStickY(),
+                  PS4.RStickX(),
+                  PS4.RStickY());
+#endif
+#if SENSORS
+    Serial.printf("gx:%5d,gy:%5d,gz:%5d,ax:%5d,ay:%5d,az:%5d\n",
+                  PS4.GyrX(),
+                  PS4.GyrY(),
+                  PS4.GyrZ(),
+                  PS4.AccX(),
+                  PS4.AccY(),
+                  PS4.AccZ());
+#endif
     lastTimeStamp = millis();
   }
 }
 
 void onDisConnect() {
-  Serial.println("Disconnected!.");
+  Serial.println("Disconnected!");
 }
