1a6f680456490c3feb456799516999196bcd0f1e
Pablo Marquez <92705318+pablomarquez76@users.noreply.github.com>
2023-11-20 15:45:13 -0300

    Update PS4Data.ino added helpful functions

--- examples/PS4Data/PS4Data.ino
+++ examples/PS4Data/PS4Data.ino
@@ -1,4 +1,9 @@
 #include <PS4Controller.h>
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_err.h"
+
 unsigned long lastTimeStamp = 0;
 #define EVENTS 0
 #define BUTTONS 0
@@ -11,11 +16,35 @@ void setup() {
   PS4.attachOnConnect(onConnect);
   PS4.attachOnDisconnect(onDisConnect);
   PS4.begin();
-  Serial.println("Ready!");
+  removePairedDevices(); // This helps to solve connection issues
+  Serial.print("This device MAC is: ");
+  printDeviceAddress();
+  Serial.println("");
 }
 
 void loop() {
   delay(100);
+}
+
+void removePairedDevices() {
+  uint8_t pairedDeviceBtAddr[20][6];
+  int count = esp_bt_gap_get_bond_device_num();
+  esp_bt_gap_get_bond_device_list(&count, pairedDeviceBtAddr);
+  for (int i = 0; i < count; i++) {
+    esp_bt_gap_remove_bond_device(pairedDeviceBtAddr[i]);
+  }
+}
+
+void printDeviceAddress() {
+  const uint8_t* point = esp_bt_dev_get_address();
+  for (int i = 0; i < 6; i++) {
+    char str[3];
+    sprintf(str, "%02x", (int)point[i]);
+    Serial.print(str);
+    if (i < 5) {
+      Serial.print(":");
+    }
+  }
 }
 
 void onConnect() {
